d95d546f655790b1338511c8977b57a9841af159
ViniciusLima20 <vynycyuss@hotmail.com.br>
2025-06-18 14:31:15 -0300

    comite01

---
+++ Main.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+
+pygame.init()
+tela=pygame.display.set_mode((800,600))
+tela.fill((255,0,0))
+icon=pygame.image.load('Data\cap.webp')
+pygame.display.set_icon(icon)
+
+
+
+pygame.display.update()
+
---
+++ at1.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+
+pygame.init()
+tela=paygame.display.set_mode((800,600))
+tela.fill((255,0,0))
+
+
+pygame.display.update()
+
+
+pygame.quit()
